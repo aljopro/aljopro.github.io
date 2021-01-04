c787caafb5ab832e8a2bcfaf712262eb66bd850b
John Chappell <aljopro@gmail.com>
2021-01-04 12:45:51 -0600

    Update site to use my new name.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-const gulp = require('gulp');
+const {src, watch, dest, series, task, parallel} = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
+
 
 const config = {
 	sass: './style/**/*.scss',
 	sassDest: './style'
 }
 
-function serve() {
+function serveInit() {
 	return browserSync.init({
 		server: {
 			baseDir: "./"
@@ -17,31 +18,34 @@ function serve() {
 }
 
 function css() {
-	return gulp.src(config.sass)
+	return src(config.sass)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
 			cascade: false
 		}))
-		.pipe(gulp.dest(config.sassDest))
+		.pipe(dest(config.sassDest))
 		.pipe(browserSync.stream({match: '**/*.css'}));
 }
 
 function cssWatch() {
-	gulp.watch(config.sass, ['css']);
+	watch(config.sass, series(['css']));
 }
 
 function htmlWatch() {
-	gulp.watch(['**/*.html', '!(node_modules)/**/*']).on('change', browserSync.reload);
+	watch(['**/*.html', '!(node_modules)/**/*']).on('change', browserSync.reload);
 }
 
 function jsWatch() {
-	gulp.watch(['**/*.js', '!(node_modules)/**/*']).on('change', browserSync.reload);
+	watch(['**/*.js', '!(node_modules)/**/*']).on('change', browserSync.reload);
 }
 
+const serve = parallel([serveInit, cssWatch, htmlWatch, jsWatch]);
 
-gulp.task('serve', ['css:watch', 'html:watch', 'js:watch'], serve);
-gulp.task('css', css);
-gulp.task('css:watch', cssWatch);
-gulp.task('html:watch', htmlWatch);
-gulp.task('js:watch', jsWatch);
+
+module.exports = {
+	default: serve,
+	serve,
+	htmlWatch,
+	css,
+	cssWatch,
+}
